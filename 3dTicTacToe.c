1ecb99c31c5e25d7d7ff4f5e230f55651946d07d
feagans <70381848+feagans@users.noreply.github.com>
2021-03-09 22:11:23 -0500

    Print cleaner board with board state and created coin toss
    Prints cleaner looking boards and prints out each board state underneath. Game states = X WON, O WON, TIE, or In progress which prints nothing.
    
    Created a coin toss to decide what user goes first and is assigned as player X.

--- 3dTicTacToe.c
+++ 3dTicTacToe.c
@@ -1,6 +1,6 @@
 // Alyssa Feagans
 // 3/9/21
-// 3D Tic-Tac-Toe
+// 3D Tic-Tac-Toe terminal game
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,9 +8,16 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <limits.h>
 
 #define NUM_BOARDS 9
 #define BOARD_SIZE 9
+
+// Game board states
+#define X 1 // X WON
+#define O 0 // O WON
+#define TIE INT_MAX
+#define INCOMPLETE INT_MIN // Game still in progress
 
 // Waits for user to hit enter so tutorial doesnt give a huge wall of text at once.
 void checkpoint(void)
@@ -20,7 +27,16 @@ void checkpoint(void)
 	while (enter != '\r' && enter != '\n') { enter = getchar(); }
 }
 
-void printBoards(int board[NUM_BOARDS][BOARD_SIZE])
+// Fill game board with all INT_MIN = empty.
+void empty_board(char board[NUM_BOARDS][BOARD_SIZE])
+{
+	for (int i = 0; i < NUM_BOARDS; i++)
+			for (int j = 0; j < BOARD_SIZE; j++)
+				board[i][j] = '\0';
+}
+
+// Prints game board out with board headers and lists each board's state.
+void printBoards(char board[NUM_BOARDS][BOARD_SIZE], int board_states[NUM_BOARDS])
 {
 	int cur_board = 0;
 	int max_board = 2;
@@ -33,21 +49,81 @@ void printBoards(int board[NUM_BOARDS][BOARD_SIZE])
 
 		// 1st row of boards
 		for (int j = cur_board; j < cur_board + 3; j++)
-			printf(" %d | %d | %d \t", board[j][0], board[j][1], board[j][2]);
-
+			printf(" %c | %c | %c \t", board[j][0], board[j][1], board[j][2]);
 		printf("\n");
+		printf("-----------\t-----------\t-----------\n");
 
 		// 2nd row of boards
 		for (int j = cur_board; j < cur_board + 3; j++)
-			printf(" %d | %d | %d \t", board[j][3], board[j][4], board[j][5]);
+			printf(" %c | %c | %c \t", board[j][3], board[j][4], board[j][5]);
 
 		printf("\n");
+		printf("-----------\t-----------\t-----------\n");
 
 		// 3rd row of boards
 		for (int j = cur_board; j < cur_board + 3; j++)
-			printf(" %d | %d | %d \t", board[j][6], board[j][7], board[j][8]);
+			printf(" %c | %c | %c \t", board[j][6], board[j][7], board[j][8]);
 
+		printf("\n");
+		// Print each board's state if not in progress.
+		char board_state1[10], board_state2[10], board_state3[10];
+
+		for (int j = cur_board; j < cur_board + 3; j++)
+		{
+			if (board_states[j] == 1)
+			{
+				if (j == cur_board)
+					strcpy(board_state1, "X WON");
+
+				else if (j == cur_board + 1)
+					strcpy(board_state2, "X WON");
+
+				else if (j == max_board)
+					strcpy(board_state3, "X WON");
+			}
+
+			else if (board_states[j] == 0)
+			{
+				if (j == cur_board)
+					strcpy(board_state1, "O WON");
+
+				else if (j == cur_board + 1)
+					strcpy(board_state2, "O WON");
+
+				else if (j == max_board)
+					strcpy(board_state3, "O WON");
+			}
+
+			else if (board_states[j] == INT_MAX)
+			{
+				if (j == cur_board)
+					strcpy(board_state1, " TIE ");
+
+				else if (j == cur_board + 1)
+					strcpy(board_state2, " TIE ");
+
+				else if (j == max_board)
+					strcpy(board_state3, " TIE ");
+			}
+
+			// If board still in progress, char will be empty.
+			else if (board_states[j] == INT_MIN)
+			{
+				if (j == cur_board)
+					strcpy(board_state1, "");
+
+				else if (j == cur_board + 1)
+					strcpy(board_state2, "");
+
+				else if (j == max_board)
+					strcpy(board_state3, "");
+			}
+		}
+
+		// Print line showing each board's current state.
+		printf("   %s  \t   %s      \t   %s\n\n\n", board_state1, board_state2, board_state3);
 		printf("\n\n");
+
 		// Next 3 boards
 		cur_board += 3;
 		max_board += 3;
@@ -55,29 +131,28 @@ void printBoards(int board[NUM_BOARDS][BOARD_SIZE])
 }
 
 // Fill board with random values 1 or 0 for testing purposes
-int fillBoards (int board[NUM_BOARDS][BOARD_SIZE])
+int fillBoards(char board[NUM_BOARDS][BOARD_SIZE])
 {
-	srand(time(0));
+	srand(time(NULL));
 	for (int i = 0; i < 9; i++)
 	{
 		for (int j = 0; j < 9; j++)
 		{
 			// Generate random number 1 to 100
-			int num = (rand() % 100) + 1;
+			int num = rand() % 100 + 1;
 			// Place an 'O'
 			if (num <= 50)
-			 	board[i][j] = 0;
+			 	board[i][j] = 'O';
 
 			// Place an 'X'
 			else
-				board[i][j] = 1;
-
+				board[i][j] = 'X';
 		}
 	}
 }
 
 // Prints out all board values for debugging.
-void printBoardValues(int board[NUM_BOARDS][BOARD_SIZE])
+void printBoardValues(char board[NUM_BOARDS][BOARD_SIZE])
 {
 	for (int i = 0; i < 9; i++)
 		for (int j = 0; j < 9; j++)
@@ -224,10 +299,93 @@ void tutorial(void)
 	checkpoint();
 }
 
+// Print head ASCII art.
+void print_heads(void)
+{
+	// Quarter ASCII art by Joan G. Stark and edited.
+	printf("\n");
+	printf("\n");
+	printf("        _.-'~~`~~'-._\n");
+	printf("     .'`  B   E   R  `'.\n");
+	printf("   / I               T  `\n");
+	printf("  /`       .-'~''-.      `;\n");
+	printf(" ; L      / `-    ]      Y |\n");
+	printf(";        />  `.  -.|       ;\n");
+	printf("|       /_     '-.__)      |\n");
+	printf("|        |-  _.' l |       |\n");
+	printf(";        `~~;     ll       ;\n");
+	printf(";  INGODWE /      Il)P     ;\n");
+	printf(" -   TRUST '.___.-'`''    /\n");
+	printf("  `                      /`\n");
+	printf("    ' ._   1 9 9 7   _.'\n");
+	printf("    	`'-..,,,..-'`\n");
+	printf("\n");
+	printf("\n");
+}
+
+// Returns true if user selected correct side of coin it randomly lands on.
+bool coin_flip(int user_selection)
+{
+	int coin;
+	srand(time(NULL));
+	rand();
+	int rando = rand() % 100 + 1;
+
+	// Coin is heads
+	if (rando <= 49)
+	{
+		coin = 0;
+		printf("\n\nCoin is HEADS\n");
+		print_heads();
+	}
+
+	// Coin is tails
+	else
+	{
+		coin = 1;
+		printf("\nCoin is TAILS\n");
+	}
+
+	// Return true if correctly guessed, false if not.
+	return (coin == user_selection);
+}
+
+// Start a new game with empty board.
+// Returns board
+void start_game(char board[NUM_BOARDS][BOARD_SIZE])
+{
+	int user_selection = 0;
+	bool coin = true;
+	int states[NUM_BOARDS] = {0, 1, 0, INT_MAX, INT_MIN, 0, 1, 1, INT_MIN};
+	printf("Starting a new game...\n\n");
+	while(coin)
+	{
+		printf("Pick heads or tails for coin flip that decides who will go first.\n");
+		printf("0: Heads\n");
+		printf("1: Tails\n");
+		scanf("%d", &user_selection);
+		sleep(2);
+		// User picked heads
+		if (user_selection == 0 || user_selection == 1)
+		{
+			if (coin_flip(user_selection))
+				printf("You go first. You are player X.\n");
+
+			else
+				printf("You go second. You are player O.\n");
+
+			coin = false;
+		}
+	}
+
+	empty_board(board);
+	printBoards(board, states);
+}
+
 int main(void)
 {
 	// Create board will be initialized with trash values.
-	int board[NUM_BOARDS][BOARD_SIZE];
+	char board[NUM_BOARDS][BOARD_SIZE];
 	int user_selection = 0;
 	bool menu = true;
 
@@ -264,7 +422,9 @@ int main(void)
 
 		// Start new game
 		else if (user_selection == 2)
-			printf("Starting a new game...\n");
+		{
+			start_game(board);
+		}
 
 		// Exit program
 		else if (user_selection == 3)
@@ -278,8 +438,5 @@ int main(void)
 		else
 			printf("Please enter a valid menu selection number.\n");
 	}
-
-	fillBoards(board);
-	printBoards(board);
 	return 1;
 }
